91cbadcd28b45504ba4d0859ba3421bdcc620887
Abigael <fambabigael38@gmail.com>
2024-05-26 15:39:43 +0300

    code

--- lib/testing/app_test.py
+++ lib/testing/app_test.py
@@ -25,13 +25,20 @@ class TestAppPy:
         prints "Hello World! Pass this test, please."
         '''
         # Capture the output of the script
-        captured_out = io.StringIO()
+        captured_out = io.BytesIO() if sys.version_info[0] == 2 else io.StringIO()
         sys.stdout = captured_out
-        runpy.run_path("lib/app.py")
-        sys.stdout = sys.__stdout__
+
+        try:
+            # Run the script
+            runpy.run_path("lib/app.py")
+        finally:
+            sys.stdout = sys.__stdout__
+
+        # Convert captured output to string
+        captured_out_str = captured_out.getvalue().decode('utf-8') if sys.version_info[0] == 2 else captured_out.getvalue()
 
         # Check if the expected string is in the captured output
-        assert captured_out.getvalue().strip() == "Hello World! Pass this test, please."
+        assert "Hello World! Pass this test, please." in captured_out_str.strip()
 
 if __name__ == "__main__":
     pytest.main()
